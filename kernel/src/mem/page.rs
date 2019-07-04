d4acbaed0750c60ffe350d3c9931796de3a947ad
Hailey Somerville <hailey@hailey.lol>
2019-07-04 13:04:10 +1000

    implement 64 bit paging in mem::page

--- kernel/src/mem/page.rs
+++ kernel/src/mem/page.rs
@@ -3,10 +3,26 @@ use bitflags::bitflags;
 use crate::critical::{self, Critical};
 use crate::mem::phys::{self, Phys, MemoryExhausted};
 
-#[repr(transparent)]
-struct Entry(u64);
-
 pub const PAGE_SIZE: usize = 0x1000;
+
+#[repr(transparent)]
+struct PmlEntry(u64);
+
+impl PmlEntry {
+    pub fn phys(&self) -> Option<u64> {
+        // TODO might need to use a different bit other than present when we
+        // start demand mapping/paging out
+        if self.flags().contains(PageFlags::PRESENT) {
+            Some(self.0 & !0xfff)
+        } else {
+            None
+        }
+    }
+
+    pub fn flags(&self) -> PageFlags {
+        PageFlags::from_bits(self.0 & 0xfff).expect("PageFlags::from_bits in PmlEntry::flags")
+    }
+}
 
 bitflags! {
     pub struct PageFlags: u64 {
@@ -16,8 +32,25 @@ bitflags! {
     }
 }
 
-const PAGE_DIRECTORY: *mut Entry = 0xfffff000 as *mut Entry;
-const PAGE_TABLES: *mut Entry = 0xffc00000 as *mut Entry;
+unsafe fn pml4_entry(virt: u64) -> *mut PmlEntry {
+    let base = 0xfffffffffffff000 as *mut PmlEntry;
+    base.add(((virt >> 39) & 0x1ff) as usize)
+}
+
+unsafe fn pml3_entry(virt: u64) -> *mut PmlEntry {
+    let base = 0xffffffffffe00000 as *mut PmlEntry;
+    base.add(((virt >> 30) & 0x3ffff) as usize)
+}
+
+unsafe fn pml2_entry(virt: u64) -> *mut PmlEntry {
+    let base = 0xffffffffc0000000 as *mut PmlEntry;
+    base.add(((virt >> 21) & 0x7ffffff) as usize)
+}
+
+unsafe fn pml1_entry(virt: u64) -> *mut PmlEntry {
+    let base = 0xffffff8000000000 as *mut PmlEntry;
+    base.add(((virt >> 12) & 0xfffffffff) as usize)
+}
 
 extern "C" {
     static mut temp_page: u8;
@@ -50,23 +83,45 @@ pub unsafe fn map(phys: Phys, virt: *const u8, flags: PageFlags) -> Result<(), M
     critical::section(|| {
         let virt = virt as u64;
 
-        let pde = PAGE_DIRECTORY.add((virt >> 22) as usize);
-        let pte = PAGE_TABLES.add((virt >> 12) as usize);
+        let pml4_ent = pml4_entry(virt);
+        let pml3_ent = pml3_entry(virt);
+        let pml2_ent = pml2_entry(virt);
+        let pml1_ent = pml1_entry(virt);
 
-        if (*pde).0 == 0 {
+        // ensure all pml tables exist:
+
+        if (*pml4_ent).0 == 0 {
             // need to allocate new page table for entry:
-            let pt = phys::alloc().map_err(|_: MemoryExhausted|
+            let pml3_tab = phys::alloc().map_err(|_: MemoryExhausted|
                 MapError::CannotAllocatePageTable)?;
 
-            *pde = Entry(pt.0 | (PageFlags::PRESENT | PageFlags::WRITE).bits());
-            invlpg(pte as *const u8);
+            *pml4_ent = PmlEntry(pml3_tab.0 | (PageFlags::PRESENT | PageFlags::WRITE).bits());
+            invlpg(pml3_ent as *const u8);
         }
 
-        if (*pte).0 != 0 {
+        if (*pml3_ent).0 == 0 {
+            // need to allocate new page table for entry:
+            let pml2_tab = phys::alloc().map_err(|_: MemoryExhausted|
+                MapError::CannotAllocatePageTable)?;
+
+            *pml3_ent = PmlEntry(pml2_tab.0 | (PageFlags::PRESENT | PageFlags::WRITE).bits());
+            invlpg(pml2_ent as *const u8);
+        }
+
+        if (*pml2_ent).0 == 0 {
+            // need to allocate new page table for entry:
+            let pml1_tab = phys::alloc().map_err(|_: MemoryExhausted|
+                MapError::CannotAllocatePageTable)?;
+
+            *pml2_ent = PmlEntry(pml1_tab.0 | (PageFlags::PRESENT | PageFlags::WRITE).bits());
+            invlpg(pml1_ent as *const u8);
+        }
+
+        if (*pml1_ent).0 != 0 {
             return Err(MapError::AlreadyMapped);
         }
 
-        *pte = Entry(phys.0 | flags.bits());
+        *pml1_ent = PmlEntry(phys.0 | flags.bits());
         invlpg(virt as *const u8);
 
         Ok(())
@@ -82,18 +137,28 @@ pub unsafe fn unmap(virt: *const u8) -> Result<(), UnmapError> {
     critical::section(|| {
         let virt = virt as u64;
 
-        let pde = PAGE_DIRECTORY.add((virt >> 22) as usize);
-        let pte = PAGE_TABLES.add((virt >> 12) as usize);
+        let pml4_ent = pml4_entry(virt);
+        let pml3_ent = pml3_entry(virt);
+        let pml2_ent = pml2_entry(virt);
+        let pml1_ent = pml1_entry(virt);
 
-        if (*pde).0 == 0 {
+        if (*pml4_ent).0 == 0 {
             return Err(UnmapError::NotMapped);
         }
 
-        if (*pte).0 == 0 {
+        if (*pml3_ent).0 == 0 {
             return Err(UnmapError::NotMapped);
         }
 
-        *pte = Entry(0);
+        if (*pml2_ent).0 == 0 {
+            return Err(UnmapError::NotMapped);
+        }
+
+        if (*pml1_ent).0 == 0 {
+            return Err(UnmapError::NotMapped);
+        }
+
+        *pml1_ent = PmlEntry(0);
 
         Ok(())
     })
