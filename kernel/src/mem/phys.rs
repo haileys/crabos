d90ce6c28d2736ff794fdf7c2d54789fb561ea2e
Hailey Somerville <hailey@hailey.lol>
2019-07-11 14:25:55 +1000

    rearrange phys mod

--- kernel/src/mem/phys.rs
+++ kernel/src/mem/phys.rs
@@ -1,9 +1,9 @@
 use core::fmt::{self, Debug};
 use core::mem;
-use core::ptr::{self, NonNull};
+use core::ptr;
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
-use crate::critical;
+use crate::critical::{self, Critical};
 use crate::mem::page::{self, PAGE_SIZE, PageFlags};
 use crate::sync::Mutex;
 
@@ -13,66 +13,12 @@ extern "C" {
 }
 
 static REF_COUNT_ENABLED: AtomicBool = AtomicBool::new(false);
-
-const MAX_PHYS_PAGE: u64 = 1 << 48;
-
-fn ref_count(raw: RawPhys) -> &'static AtomicUsize {
-    if raw.0 > MAX_PHYS_PAGE {
-        panic!("addr > MAX_PHYS_PAGE (addr = {:x?})", raw);
-    }
-
-    let base = unsafe { &_phys_rc as *const AtomicUsize };
-    let end = unsafe { &_phys_rc_end as *const AtomicUsize };
-
-    let page_number = raw.0 >> 12;
-    let rc = unsafe { base.add(page_number as usize) };
-
-    if rc > end {
-        panic!("rc > end");
-    }
-
-    unsafe { &*rc }
-}
-
-fn inc_ref(raw: RawPhys) {
-    if REF_COUNT_ENABLED.load(Ordering::SeqCst) {
-        ref_count(raw)
-            // TODO - we can probably do better than SeqCst here:
-            .fetch_add(1, Ordering::SeqCst);
-    }
-}
-
-enum PhysStatus {
-    InUse,
-    ShouldFree,
-}
-
-fn dec_ref(raw: RawPhys) -> PhysStatus {
-    if REF_COUNT_ENABLED.load(Ordering::SeqCst) {
-        let previous_ref_count = ref_count(raw)
-            // TODO - we can probably do better than SeqCst here:
-            .fetch_sub(1, Ordering::SeqCst);
-
-        if previous_ref_count == 0 {
-            panic!("phys::dec_ref underflowed!");
-        }
-
-        // return the current ref count as of immediately after this operation:
-        if previous_ref_count == 1 {
-            PhysStatus::ShouldFree
-        } else {
-            PhysStatus::InUse
-        }
-    } else {
-        PhysStatus::InUse
-    }
-}
-
 static PHYS_REGIONS: Mutex<Option<[PhysRegion; 8]>> = Mutex::new(None);
 static mut NEXT_FREE_PHYS: Option<RawPhys> = None;
 
 const REGION_KIND_USABLE: u32 = 1;
 const HIGH_MEMORY_BOUNDARY: RawPhys = RawPhys(0x100000);
+const MAX_PHYS_PAGE: u64 = 1 << 48;
 
 #[derive(Debug)]
 pub struct MemoryExhausted;
@@ -134,6 +80,145 @@ pub struct BiosMemoryRegion {
     size: u64,
     kind: u32,
     acpi_ex_attrs: u32,
+}
+
+fn alloc_freelist() -> Option<Phys> {
+    let crit = critical::begin();
+
+    unsafe {
+        if let Some(phys) = NEXT_FREE_PHYS.take() {
+            let phys = Phys::new(phys);
+            let mapped = page::temp_map::<Option<RawPhys>>(RawPhys(phys.0), &crit);
+
+            // pull linked next free phys out:
+            NEXT_FREE_PHYS = (*mapped).take();
+
+            // zero page before returning:
+            ptr::write_bytes(mapped as *mut u64, 0, PAGE_SIZE / mem::size_of::<u64>());
+
+            page::temp_unmap(&crit);
+
+            Some(phys)
+        } else {
+            None
+        }
+    }
+}
+
+fn alloc_new(regions: &mut [PhysRegion]) -> Result<Phys, MemoryExhausted> {
+    for region in regions {
+        if region.size == 0 {
+            continue;
+        }
+
+        let raw_phys = region.begin;
+        region.begin.0 += PAGE_SIZE as u64;
+        region.size -= PAGE_SIZE as u64;
+
+        let phys = unsafe { Phys::new(raw_phys) };
+
+        unsafe {
+            let crit = critical::begin();
+
+            let mapped = page::temp_map::<u64>(raw_phys, &crit);
+            ptr::write_bytes(mapped, 0, PAGE_SIZE / mem::size_of::<u64>());
+            page::temp_unmap(&crit);
+        }
+
+        return Ok(phys);
+    }
+
+    Err(MemoryExhausted)
+}
+
+pub fn alloc() -> Result<Phys, MemoryExhausted> {
+    if let Some(page) = alloc_freelist() {
+        return Ok(page);
+    }
+
+    alloc_new(PHYS_REGIONS.lock()
+        .as_mut()
+        .expect("PHYS_REGIONS is None in phys::alloc"))
+}
+
+impl Drop for Phys {
+    fn drop(&mut self) {
+        match dec_ref(RawPhys(self.0)) {
+            PhysStatus::InUse => {}
+            PhysStatus::ShouldFree => {
+                let crit = critical::begin();
+
+                unsafe {
+                    let link = page::temp_map::<Option<RawPhys>>(RawPhys(self.0), &crit);
+                    ptr::write(link, NEXT_FREE_PHYS.take());
+                    page::temp_unmap(&crit);
+
+                    NEXT_FREE_PHYS = Some(RawPhys(self.0));
+                }
+            }
+        }
+    }
+}
+
+pub unsafe fn init_ref_counts(_critical: &Critical) {
+    // inc ref for all currently mapped pages
+    page::each_phys(|raw_phys| {
+        inc_ref(raw_phys);
+    });
+
+    REF_COUNT_ENABLED.store(true, Ordering::SeqCst);
+}
+
+fn ref_count(raw: RawPhys) -> &'static AtomicUsize {
+    if raw.0 > MAX_PHYS_PAGE {
+        panic!("addr > MAX_PHYS_PAGE (addr = {:x?})", raw);
+    }
+
+    let base = unsafe { &_phys_rc as *const AtomicUsize };
+    let end = unsafe { &_phys_rc_end as *const AtomicUsize };
+
+    let page_number = raw.0 >> 12;
+    let rc = unsafe { base.add(page_number as usize) };
+
+    if rc > end {
+        panic!("rc > end");
+    }
+
+    unsafe { &*rc }
+}
+
+fn inc_ref(raw: RawPhys) {
+    if REF_COUNT_ENABLED.load(Ordering::SeqCst) {
+        ref_count(raw)
+            // TODO - we can probably do better than SeqCst here:
+            .fetch_add(1, Ordering::SeqCst);
+    }
+}
+
+enum PhysStatus {
+    InUse,
+    ShouldFree,
+}
+
+fn dec_ref(raw: RawPhys) -> PhysStatus {
+    if REF_COUNT_ENABLED.load(Ordering::SeqCst) {
+        let previous_ref_count = ref_count(raw)
+            // TODO - we can probably do better than SeqCst here:
+            .fetch_sub(1, Ordering::SeqCst);
+
+        if previous_ref_count == 0 {
+            panic!("phys::dec_ref underflowed!");
+        }
+
+        // return the current ref count as of immediately after this operation:
+        if previous_ref_count == 1 {
+            PhysStatus::ShouldFree
+        } else {
+            PhysStatus::InUse
+        }
+    } else {
+        PhysStatus::InUse
+    }
 }
 
 unsafe fn ensure_rc_page(phys: RawPhys) {
@@ -234,91 +319,4 @@ pub unsafe extern "C" fn phys_init(bios_memory_map: *const BiosMemoryRegion, reg
     }
 
     crate::println!();
-}
-
-pub unsafe fn init_ref_counts() {
-    // inc ref for all currently mapped pages
-    page::each_phys(|raw_phys| {
-        inc_ref(raw_phys);
-    });
-
-    REF_COUNT_ENABLED.store(true, Ordering::SeqCst);
-}
-
-fn alloc_freelist() -> Option<Phys> {
-    let crit = critical::begin();
-
-    unsafe {
-        if let Some(phys) = NEXT_FREE_PHYS.take() {
-            let phys = Phys::new(phys);
-            let mapped = page::temp_map::<Option<RawPhys>>(RawPhys(phys.0), &crit);
-
-            // pull linked next free phys out:
-            NEXT_FREE_PHYS = (*mapped).take();
-
-            // zero page before returning:
-            ptr::write_bytes(mapped as *mut u64, 0, PAGE_SIZE / mem::size_of::<u64>());
-
-            page::temp_unmap(&crit);
-
-            Some(phys)
-        } else {
-            None
-        }
-    }
-}
-
-fn alloc_new(regions: &mut [PhysRegion]) -> Result<Phys, MemoryExhausted> {
-    for region in regions {
-        if region.size == 0 {
-            continue;
-        }
-
-        let raw_phys = region.begin;
-        region.begin.0 += PAGE_SIZE as u64;
-        region.size -= PAGE_SIZE as u64;
-
-        let phys = unsafe { Phys::new(raw_phys) };
-
-        unsafe {
-            let crit = critical::begin();
-
-            let mapped = page::temp_map::<u64>(raw_phys, &crit);
-            ptr::write_bytes(mapped, 0, PAGE_SIZE / mem::size_of::<u64>());
-            page::temp_unmap(&crit);
-        }
-
-        return Ok(phys);
-    }
-
-    Err(MemoryExhausted)
-}
-
-pub fn alloc() -> Result<Phys, MemoryExhausted> {
-    if let Some(page) = alloc_freelist() {
-        return Ok(page);
-    }
-
-    alloc_new(PHYS_REGIONS.lock()
-        .as_mut()
-        .expect("PHYS_REGIONS is None in phys::alloc"))
-}
-
-impl Drop for Phys {
-    fn drop(&mut self) {
-        match dec_ref(RawPhys(self.0)) {
-            PhysStatus::InUse => {}
-            PhysStatus::ShouldFree => {
-                let crit = critical::begin();
-
-                unsafe {
-                    let link = page::temp_map::<Option<RawPhys>>(RawPhys(self.0), &crit);
-                    ptr::write(link, NEXT_FREE_PHYS.take());
-                    page::temp_unmap(&crit);
-
-                    NEXT_FREE_PHYS = Some(RawPhys(self.0));
-                }
-            }
-        }
-    }
 }
