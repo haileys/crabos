01e9c28cdb3c03ecc19fff7608e9d71128a8c55d
Hailey Somerville <hailey@hailey.lol>
2019-07-15 02:07:34 +1000

    abstract return type of console::get

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -3,24 +3,8 @@ use core::marker::PhantomData;
 
 use crate::critical::Critical;
 
-// TODO - unsafe. we need to lock around access to PortE9
-pub struct PortE9<'a> {
-    crit: PhantomData<&'a Critical>,
-}
-
-// holding a valid lifetime of Critical proves we're in a critical section:
-pub fn get<'a>(_crit: &'a Critical) -> PortE9<'a> {
+pub fn get<'a>(_crit: &'a Critical) -> impl Write + 'a {
     PortE9 { crit: PhantomData }
-}
-
-impl<'a> Write for PortE9<'a> {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for b in s.as_bytes() {
-            unsafe { asm!("outb %al, %dx" :: "{dx}"(0xe9), "{al}"(*b) :: "volatile"); }
-        }
-
-        Ok(())
-    }
 }
 
 #[macro_export]
@@ -40,4 +24,18 @@ macro_rules! print {
 pub fn write_fmt(args: fmt::Arguments, crit: &Critical) {
     let mut con = get(&crit);
     let _ = fmt::write(&mut con, args);
+}
+
+struct PortE9<'a> {
+    crit: PhantomData<&'a Critical>,
+}
+
+impl<'a> Write for PortE9<'a> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for b in s.as_bytes() {
+            unsafe { asm!("outb %al, %dx" :: "{dx}"(0xe9), "{al}"(*b) :: "volatile"); }
+        }
+
+        Ok(())
+    }
 }
