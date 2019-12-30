afed55abc3a7dd0dfdff4224eda70f425f3af59c
Hailey Somerville <hailey@hailey.lol>
2019-12-30 18:15:16 +1100

    remove unneeded features from alloc_collections

--- alloc_collections/src/lib.rs
+++ alloc_collections/src/lib.rs
@@ -23,7 +23,6 @@
 #![feature(receiver_trait)]
 #![feature(slice_from_raw_parts)]
 #![feature(specialization)]
-// #![feature(staged_api)]
 #![feature(std_internals)]
 #![feature(str_internals)]
 #![feature(trusted_len)]
@@ -33,12 +32,10 @@
 #![feature(unsized_locals)]
 #![feature(allocator_internals)]
 #![feature(internal_uninit_const)]
-#![feature(rustc_const_unstable)]
 #![feature(slice_partition_dedup)]
 #![feature(maybe_uninit_extra, maybe_uninit_slice)]
 #![feature(alloc_layout_extra)]
 #![feature(try_trait)]
-#![feature(never_type)]
 
 #[macro_use]
 extern crate core;
