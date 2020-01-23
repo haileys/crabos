08232c5a722c9872cf5fc0a7bfa4ecc06e2fd9b1
Hailey Somerville <hailey@hailey.lol>
2020-01-24 00:00:58 +1100

    pull framebuffer base address out of VBE mode info struct

--- userland/init.asm
+++ userland/init.asm
@@ -1,13 +1,27 @@
 global main
 
 extern syscall_map_physical_memory
+extern syscall_release_page
 
 main:
+    ; map VBE mode info in low memory:
+    mov rdi, 0x00006000
+    mov rsi, 0x00006000
+    mov rdx, 1
+    mov rcx, 0 ; read only
+    call syscall_map_physical_memory
+
     mov rdi, 0xe0000000
-    mov rsi, 0xe0000000
+    ; read offset 40 of VBE mode info struct offset for phys_base
+    mov rsi, [0x6000 + 40]
     mov rdx, 8 * 1024 * 1024 / 4096
     mov rcx, 2 ; WRITE
     call syscall_map_physical_memory
+
+    ; unmap VBE mode info
+    mov rdi, 0x6000
+    mov rsi, 1
+    call syscall_release_page
 
     mov r8, 0
     mov rdi, 0xe0000000
