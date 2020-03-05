5175c17c612fa9f0318cd89b3d14feca95fdcd76
Hailey Somerville <hailey@hailey.lol>
2020-03-05 17:36:47 +1100

    impl panic in crabapi

--- userland/crabapi/src/lib.rs
+++ userland/crabapi/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(asm)]
 #![feature(core_panic)]
+#![feature(panic_info_message)]
 #![feature(start)]
 
 mod panic;
--- userland/crabapi/src/panic.rs
+++ userland/crabapi/src/panic.rs
@@ -1,10 +1,44 @@
+use arrayvec::ArrayString;
+use core::fmt::{self, Write};
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    unsafe {
-        asm!("xchgw %bx, %bx" :: "rax"(0xf0f0f0f0_f0f0f0f0u64));
-    }
+use crate::syscall;
 
-    loop {}
+const CONSOLE_HANDLE: u64 = 1;
+
+unsafe fn write_bytes(buf: &[u8]) {
+    syscall::write_file(CONSOLE_HANDLE, buf.as_ptr(), buf.len() as u64);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        match info.message() {
+            Some(args) => {
+                write_bytes(b"panic: ");
+                let mut buff = ArrayString::<[u8; 1024]>::new();
+                let _ = fmt::write(&mut buff, *args);
+                write_bytes(buff.as_bytes());
+                write_bytes(b"\n");
+            }
+            None => {
+                write_bytes(b"panic\n");
+            }
+        }
+
+        if let Some(loc) = info.location() {
+            write_bytes(b" at ");
+            write_bytes(loc.file().as_bytes());
+            write_bytes(b":");
+
+            let mut buff = ArrayString::<[u8; 16]>::new();
+            let _ = write!(&mut buff, "{}", loc.line());
+            write_bytes(buff.as_bytes());
+
+            write_bytes(b"\n");
+        }
+
+        syscall::exit(u64::max_value());
+        loop {}
+    }
 }
